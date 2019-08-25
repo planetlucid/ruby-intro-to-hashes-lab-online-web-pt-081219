b424145e38d80aafa8dd3648b8912a18895080cf
Ryan S <planetlucid@gmail.com>
2019-08-25 20:26:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,6 +19,7 @@ def monopoly_with_second_tier
 	  names:{
 	    reading_railroad:{},
 	    pennsylvania_railroad:{},
+	    b_and_o_railroad:{},
 	  }, 
 	  rent_in_dollars:{one_piece_owned: 25},
 	  {two_pieces_owned:50},
